3195c9229fa96f7846305f8d84af5107977e5df6
Tiferanmi <boluwatiferanmiomoniyi@gmail.com>
2023-04-23 13:16:12 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
-* main - using printf function
-* Return: Always 0 (Success)
-*/
+ * main - using printf function
+ * Return: 0(Success)
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n")
