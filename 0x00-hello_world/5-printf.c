5fa6bb3f9dabb489f5a77885491b638045207c93
Tiferanmi <boluwatiferanmiomoniyi@gmail.com>
2023-04-23 12:36:08 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A program that print a line using print function
- * Return: 0 (Success)
+ * main - A program that print a line using printf function
+ * Return: 0(Success)
  */
 int main(void)
 {
