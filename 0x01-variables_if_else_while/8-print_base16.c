45319201b985774b167d297abc47a1220d3d4571
Tiferanmi <boluwatiferanmiomoniyi@gmail.com>
2023-06-02 06:18:23 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 
 /**
- * main - prints the lowercase alphabet in reverse,
- * followed by a new line
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char ch;
+	int i;
 
-	for (ch = 'z'; ch >= 'a'; ch--)
+	for (i = 48; i < 58; i++)
 	{
-		putchar(ch);
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
 }
-
